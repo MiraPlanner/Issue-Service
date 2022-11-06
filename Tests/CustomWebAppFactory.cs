bbc3a9e299db39473a9f327bae421ebf85385a6c
Milan <58393068+Desoxyr@users.noreply.github.com>
2022-11-06 16:22:02 +0100

    Add database connection

---
+++ Tests/CustomWebAppFactory.cs
@@ -0,0 +1,33 @@
+﻿
+
+using Castle.Core.Configuration;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.DependencyInjection;
+using Mira_Common.Settings;
+using MongoDB.Driver;
+
+namespace Tests;
+
+public class CustomWebAppFactory<TEntryPoint> : WebApplicationFactory<Program> where TEntryPoint : Program
+{
+    private readonly IntegrationDb _integrationDb;
+    
+    public CustomWebAppFactory()
+    {
+        _integrationDb = new IntegrationDb();
+    }
+    
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureServices(services =>
+        {
+            var descriptor = services.SingleOrDefault(
+                d => d.ServiceType ==
+                     typeof(MongoDB.Driver.IMongoDatabase));
+            if (descriptor != null)
+                services.Remove(descriptor);
+            services.AddSingleton(_ => _integrationDb.Database);
+        });
+    }
+}
---
+++ Tests/IssueIntegrationTest.cs
@@ -0,0 +1,32 @@
+﻿using System.Net.Http.Json;
+using Issue_Service.Dtos;
+using Sprint_Service.Models;
+
+namespace Tests;
+
+public class IssueIntegrationTest : IClassFixture<CustomWebAppFactory<Program>>
+{
+    private readonly HttpClient _client;
+    public IssueIntegrationTest(CustomWebAppFactory<Program> factory) 
+        => _client = factory.CreateClient();
+    
+    [Fact]
+    public async Task Index_WhenCalled_ReturnsApplicationForm()
+    {
+        CreateIssueDto createIssueDto = new CreateIssueDto(
+            new Guid(),
+            "title",
+            "description",
+            0,
+            IssueStatus.ToDo,
+            IssueType.Task
+        );
+        
+        var create = await _client.PostAsJsonAsync("/issues", createIssueDto);
+        
+        var response = await _client.GetAsync($"/issues/{new Guid()}");
+        response.EnsureSuccessStatusCode();
+        var responseString = await response.Content.ReadAsStringAsync();
+        Assert.Null(responseString);
+    }
+}
